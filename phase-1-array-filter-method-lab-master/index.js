310cfd0068da17d1425f362a95207efddf2423ea
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-12 14:55:25 -0400

    phase-1-array-filter-method-lab/
    phase-1-array-filter-method-lab/

--- phase-1-array-filter-method-lab-master/index.js
+++ phase-1-array-filter-method-lab-master/index.js
@@ -1,75 +1,42 @@
 function findMatching(drivers, string) {
-    let matchingDrivers = [];
-    let lowercaseString = string.toLowerCase();
-    for (let i = 0; i < drivers.length; i++) {
-      let lowercaseDriver = drivers[i].toLowerCase();
-      if (lowercaseDriver === lowercaseString) {
-        matchingDrivers.push(drivers[i]);
-      }
+  let matchingDrivers = [];
+  let lowercaseString = string.toLowerCase();
+  for (let i = 0; i < drivers.length; i++) {
+    let lowercaseDriver = drivers[i].toLowerCase();
+    if (lowercaseDriver === lowercaseString) {
+      matchingDrivers.push(drivers[i]);
     }
-    return matchingDrivers;
   }
-  
-  function fuzzyMatch(drivers, matchingDrivers) {
-    let lowercaseString = string.toLowerCase();
-    for (let i = 0; i < drivers.length; i++) {
-      let lowercaseDriver = drivers[i].toLowerCase();
-      if (lowercaseDriver.startsWith(lowercaseString)) {
-        matchingDrivers.push(drivers[i]);
-      }
+  return matchingDrivers;
+}
+
+function fuzzyMatch(drivers, string) {
+  let matchingDrivers = [];
+  let lowercaseString = string.toLowerCase();
+  for (let i = 0; i < drivers.length; i++) {
+    let lowercaseDriver = drivers[i].toLowerCase();
+    if (lowercaseDriver.startsWith(lowercaseString)) {
+      matchingDrivers.push(drivers[i]);
     }
-    return matchingDrivers;
   }
-  
-  function matchName(drivers, string) {
-    let matchingDrivers = [];
-    let lowercaseString = string.toLowerCase();
-    for (let i = 0; i < drivers.length; i++) {
-      let lowercaseName = drivers[i].name.toLowerCase();
-      if (lowercaseName === lowercaseString) {
-        matchingDrivers.push(drivers[i]);
-      }
+  return matchingDrivers;
+}
+
+function matchName(drivers, string) {
+  let matchingDrivers = [];
+  let lowercaseString = string.toLowerCase();
+  for (let i = 0; i < drivers.length; i++) {
+    let lowercaseName = drivers[i].name.toLowerCase();
+    if (lowercaseName === lowercaseString) {
+      matchingDrivers.push(drivers[i]);
     }
-    return matchingDrivers;
   }
-  
-  module.exports = {
-    findMatching,
-    fuzzyMatch,
-    matchName,
-  };
-  
-//pseudocode for the three functions:
-  
-  //function findMatching(drivers, string):
-    //matchingDrivers = []
-    //lowercaseString = string.toLowerCase()
-    //for each driver in drivers:
-      //lowercaseDriver = driver.toLowerCase()
-      //if lowercaseDriver is equal to lowercaseString:
-       //add driver to matchingDrivers
-    //return matchingDrivers
-  //function fuzzyMatch(drivers, string):
-    //matchingDrivers = []
-    //lowercaseString = string.toLowerCase()
-    //for each driver in drivers:
-     // lowercaseDriver = driver.toLowerCase()
-      //if lowercaseDriver starts with lowercaseString:
-        //add driver to matchingDrivers
-    //return matchingDrivers
-  //function matchName(drivers, string):
-    //matchingDrivers = []
-    //lowercaseString = string.toLowerCase()
-    //for each driver in drivers:
-      //lowercaseName = driver.name.toLowerCase()
-      //if lowercaseName is equal to lowercaseString:
-       // add driver to matchingDrivers
-    //return matchingDrivers
+  return matchingDrivers;
+}
 
-
-
-
-
-
-
+module.exports = {
+  findMatching,
+  fuzzyMatch,
+  matchName,
+};
 
